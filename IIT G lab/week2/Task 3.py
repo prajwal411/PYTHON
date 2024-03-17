616ca0e0c2ee6c80189e35c0cc650a3afafd2592
prajwal411 <138669849+prajwal411@users.noreply.github.com>
2024-03-17 13:44:37 +0530

    Add files via upload

---
+++ IIT G lab/week2/Task 3.py
@@ -0,0 +1,4 @@
+#Area of circle calculator
+radius=float(input("Enter the radius: "))
+Area=3.14*radius*radius
+print(F"{Area}")
---
+++ IIT G lab/week2/task 2.py
@@ -0,0 +1,3 @@
+Celsius=float(input("Enter the temperture: "))
+Fahrenheit=9/5*Celsius+32
+print(f"Converted temperture {Fahrenheit}")
